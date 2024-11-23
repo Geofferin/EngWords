e79c447f9cc187fa0c2e29ac9f9f77747d5ccdc6
geofferin <syanczov@bk.ru>
2024-11-23 17:04:19 +0300

    сменил задний фон (теперь он всратый)

--- english/english/settings.py
+++ english/english/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

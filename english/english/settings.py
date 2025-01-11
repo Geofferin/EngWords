f731f4360365c15c71ba623096dd0f14cf9c004a
geofferin <syanczov@bk.ru>
2025-01-11 12:24:34 +0300

    Перешел на postgre. Будет работать только если на компе установлен postgresql и создана база с такими же параметрами, как в settings.py.
     Для быстрого просмотра оставил так же sqllite. Для этого варианта нужно закоментировать данные бд postgre и раскоментировать данные sqllite

--- english/english/settings.py
+++ english/english/settings.py
@@ -81,10 +81,19 @@ WSGI_APPLICATION = 'english.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'engwords_db',
+        'USER': 'eng_words',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # MEDIA_ROOT = os.path(BASE_DIR, 'media')
